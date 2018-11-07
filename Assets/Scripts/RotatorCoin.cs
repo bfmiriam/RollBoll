2a0afe51c7e7e5dcac702dc0d15b3872556f628a
mbacelofernandez <mbacelofernandez@perlanegra12.danielcastelao.org>
2018-11-07 10:19:47 +0100

    Proyecto

---
+++ Assets/Scripts/RotatorCoin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatorCoin : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(00, 80, 00) * Time.deltaTime);
+    }
+}
